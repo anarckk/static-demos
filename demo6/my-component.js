8b61baa4f3befe0bee33cf9f5be85fdd7165b069
anarckk <anarckk@gmail.com>
2024-05-24 23:32:13 +0800

    1

--- demo6/my-component.js
+++
@@ -1,9 +0,0 @@
-// my-component.js
-import { ref } from 'vue'
-export default {
-  setup() {
-    const count = ref(0)
-    return { count }
-  },
-  template: /*html*/`<div>Count is: {{ count }}</div>`
-}
